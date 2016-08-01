7438059bb44d02f04b036bee32e7ca9250ce70eb
blacktop <blacktop@users.noreply.github.com>
2016-08-01 13:36:15 -0600

    move to new style

---
+++ 5.0/elastic-entrypoint.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch tini -- "$@"
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'kopf' -a "$(id -u)" = '0' ]; then
+	# Install kopf plugin
+	plugin install lmenezes/elasticsearch-kopf/v2.1.1
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
+	# Change node into a master node
+	echo "node.master: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.client: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'client' -a "$(id -u)" = '0' ]; then
+	# Change node into a client node
+	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.client: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Install kopf plugin
+	plugin install lmenezes/elasticsearch-kopf/v2.1.1
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.client: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
---
+++ CHANGELOG.md
@@ -0,0 +1,46 @@
+Change Log
+==========
+
+All notable changes to this project will be documented in this file.
+
+[5.0] - 2016-08-01
+------------------
+
+### Fixed
+
+### Added
+
+-	krallin/tini
+-	tianon/gosu
+
+### Removed
+
+### Changed
+
+[2.3] - 2016-08-01
+------------------
+
+### Fixed
+
+### Added
+
+-	krallin/tini
+-	tianon/gosu
+
+### Removed
+
+### Changed
+
+[1.7] - 2016-08-01
+------------------
+
+### Fixed
+
+### Added
+
+-	krallin/tini
+-	tianon/gosu
+
+### Removed
+
+### Changed
