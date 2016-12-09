6a66087510ef4e6c2876cdc90cd80b03dd928cfb
blacktop <blacktop@users.noreply.github.com>
2016-12-08 22:09:48 -0700

    add ES 5.1.1

---
+++ 5.1/elastic-entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch /sbin/tini -- "$@"
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
+	# Change node into a master node
+	echo "node.master: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.ingest: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch /sbin/tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'ingest' -a "$(id -u)" = '0' ]; then
+	# Change node into a client node
+	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.ingest: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch /sbin/tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.ingest: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "node.data: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+
+	set -- gosu elasticsearch /sbin/tini -- elasticsearch
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 docker-elasticsearch-alpine
 ===========================
 
-[![CircleCI](https://circleci.com/gh/blacktop/docker-elasticsearch-alpine.png?style=shield)](https://circleci.com/gh/blacktop/docker-elasticsearch-alpine) [![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org) [![Docker Stars](https://img.shields.io/docker/stars/blacktop/elasticsearch.svg)](https://hub.docker.com/r/blacktop/elasticsearch/) [![Docker Pulls](https://img.shields.io/docker/pulls/blacktop/elasticsearch.svg)](https://hub.docker.com/r/blacktop/elasticsearch/) [![Docker Image](https://img.shields.io/badge/docker image-149.7 MB-blue.svg)](https://hub.docker.com/r/blacktop/elasticsearch/)
+[![CircleCI](https://circleci.com/gh/blacktop/docker-elasticsearch-alpine.png?style=shield)](https://circleci.com/gh/blacktop/docker-elasticsearch-alpine) [![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org) [![Docker Stars](https://img.shields.io/docker/stars/blacktop/elasticsearch.svg)](https://hub.docker.com/r/blacktop/elasticsearch/) [![Docker Pulls](https://img.shields.io/docker/pulls/blacktop/elasticsearch.svg)](https://hub.docker.com/r/blacktop/elasticsearch/) [![Docker Image](https://img.shields.io/badge/docker image--blue.svg)](https://hub.docker.com/r/blacktop/elasticsearch/)
 
 Alpine Linux based [Elasticsearch](https://www.elastic.co/products/elasticsearch) Docker Image
 
