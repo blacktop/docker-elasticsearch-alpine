09db41ba05c8af000f609964974d3b335994b0af
blacktop <blacktop@users.noreply.github.com>
2017-01-01 16:22:09 -0700

    update entrypoints and configs

--- 2.3/elastic-entrypoint.sh
+++ 2.3/elastic-entrypoint.sh
@@ -7,72 +7,91 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
 fi
 
+################################################
+# = MASTER NODE =                              #
+################################################
+if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'true'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
+################################################
+# = CLIENT NODE =                              #
+################################################
+if [ "$1" = 'client' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.client:).*!\2 'true'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
+	# Set master.node's name
+	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
+		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
+	fi
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
+################################################
+# = DATA NODE =                                #
+################################################
+if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'true'!" $CONFIG
+	# Set master.node's name
+	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
+		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
+	fi
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	# Change the ownership of user-mutable directories to elasticsearch
 	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
 
-	set -- su-exec elasticsearch tini -- "$@"
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+	set -- su-exec elasticsearch "$@"
 fi
 
-if [ "$1" = 'kopf' -a "$(id -u)" = '0' ]; then
-	# Install kopf plugin
-	plugin install lmenezes/elasticsearch-kopf/v2.1.1
-
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-
-	set -- su-exec elasticsearch tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
-	# Change node into a master node
-	echo "node.master: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.client: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-
-	set -- su-exec elasticsearch tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'client' -a "$(id -u)" = '0' ]; then
-	# Change node into a client node
-	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.client: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Install kopf plugin
-	plugin install lmenezes/elasticsearch-kopf/v2.1.1
-
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-
-	set -- su-exec elasticsearch tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
-	# Change node into a data node
-	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.client: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-
-	set -- su-exec elasticsearch tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-# As argument is not related to elasticsearch,
-# then assume that user wants to run his own process,
-# for example a `bash` shell to explore this image
 exec "$@"
--- geoip/elastic-entrypoint.sh
+++ geoip/elastic-entrypoint.sh
@@ -7,96 +7,91 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
 fi
 
+################################################
+# = MASTER NODE =                              #
+################################################
+if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'true'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
+################################################
+# = INGEST NODE =                              #
+################################################
+if [ "$1" = 'ingest' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'true'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
+	# Set master.node's name
+	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
+		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
+	fi
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
+################################################
+# = DATA NODE =                                #
+################################################
+if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
+	# Change node into a data node
+	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
+	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
+	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'true'!" $CONFIG
+	# Set master.node's name
+	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
+		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
+	fi
+
+	# Drop root privileges if we are running elasticsearch
+	# allow the container to be started with `--user`
+	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+		# Change the ownership of user-mutable directories to elasticsearch
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+		chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+
+		set -- su-exec elasticsearch "$@"
+	fi
+
+	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
+fi
+
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of user-mutable directories to elasticsearch
-	for path in \
-		/usr/share/elasticsearch/data \
-		/usr/share/elasticsearch/logs \
-	; do
-		chown -R elasticsearch:elasticsearch "$path"
-	done
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
 
 	set -- su-exec elasticsearch "$@"
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
-	# Change node into a master node
-	echo "node.master: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.ingest: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Drop root privileges if we are running elasticsearch
-	# allow the container to be started with `--user`
-	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-		# Change the ownership of user-mutable directories to elasticsearch
-		for path in \
-			/usr/share/elasticsearch/data \
-			/usr/share/elasticsearch/logs \
-		; do
-			chown -R elasticsearch:elasticsearch "$path"
-		done
-
-		set -- su-exec elasticsearch "$@"
-		#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-	fi
-
-	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'ingest' -a "$(id -u)" = '0' ]; then
-	# Change node into a ingest node
-	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.ingest: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Drop root privileges if we are running elasticsearch
-	# allow the container to be started with `--user`
-	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-		# Change the ownership of user-mutable directories to elasticsearch
-		for path in \
-			/usr/share/elasticsearch/data \
-			/usr/share/elasticsearch/logs \
-		; do
-			chown -R elasticsearch:elasticsearch "$path"
-		done
-
-		set -- su-exec elasticsearch "$@"
-		#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-	fi
-
-	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
-	# Change node into a data node
-	echo "node.master: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.ingest: false" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "node.data: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-	echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> /usr/share/elasticsearch/config/elasticsearch.yml
-
-	# Drop root privileges if we are running elasticsearch
-	# allow the container to be started with `--user`
-	if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-		# Change the ownership of user-mutable directories to elasticsearch
-		for path in \
-			/usr/share/elasticsearch/data \
-			/usr/share/elasticsearch/logs \
-		; do
-			chown -R elasticsearch:elasticsearch "$path"
-		done
-
-		set -- su-exec elasticsearch "$@"
-		#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-	fi
-
-	set -- su-exec elasticsearch /sbin/tini -- elasticsearch
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
 fi
 
 exec "$@"
