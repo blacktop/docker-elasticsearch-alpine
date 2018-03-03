da9e0721088e4448740d32a6a63c39c301cff454
blacktop <blacktop@users.noreply.github.com>
2018-03-02 19:53:22 -0700

    should fix #5

--- 6.2/elastic-entrypoint.sh
+++ 6.2/elastic-entrypoint.sh
@@ -2,95 +2,43 @@
 
 set -e
 
-es_opts=''
+umask 0002
+
+declare -a es_opts
 
 while IFS='=' read -r envvar_key envvar_value
 do
     # Elasticsearch env vars need to have at least two dot separated lowercase words, e.g. `cluster.name`
-    if [[ "$envvar_key" =~ ^[a-z]+\.[a-z]+ ]]
-    then
+    if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ ]]; then
         if [[ ! -z $envvar_value ]]; then
           es_opt="-E${envvar_key}=${envvar_value}"
-          es_opts+=" ${es_opt}"
+          es_opts+=("${es_opt}")
         fi
     fi
 done < <(env)
 
 export ES_JAVA_OPTS="-Des.cgroups.hierarchy.override=/ $ES_JAVA_OPTS"
 
+# Determine if x-pack is enabled
+if bin/elasticsearch-plugin list -s | grep -q x-pack; then
+    if [[ -n "$ELASTIC_PASSWORD" ]]; then
+        [[ -f config/elasticsearch.keystore ]] ||  bin/elasticsearch-keystore create
+        echo "$ELASTIC_PASSWORD" | bin/elasticsearch-keystore add -x 'bootstrap.password'
+    fi
+fi
+
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@" ${es_opts}
-fi
-
-################################################
-# = MASTER NODE =                              #
-################################################
-if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
-	# Change node into a data node
-	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
-	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'true'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
-
-	# Change the ownership of user-mutable directories to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
-
-	set -- su-exec elasticsearch "$@" ${es_opts}
-fi
-
-################################################
-# = INGEST NODE =                              #
-################################################
-if [ "$1" = 'ingest' -a "$(id -u)" = '0' ]; then
-	# Change node into a data node
-	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
-	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'true'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'false'!" $CONFIG
-	# Set master.node's name
-	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
-		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
-	fi
-
-	# Change the ownership of user-mutable directories to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
-
-	set -- su-exec elasticsearch "$@" ${es_opts}
-fi
-
-################################################
-# = DATA NODE =                                #
-################################################
-if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
-	# Change node into a data node
-	CONFIG=/usr/share/elasticsearch/config/elasticsearch.yml
-	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 'false'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 'false'!" $CONFIG
-	sed -ri "s!^(\#\s*)?(node\.data:).*!\2 'true'!" $CONFIG
-	# Set master.node's name
-	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
-		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\"]" >> $CONFIG
-	fi
-
-	# Change the ownership of user-mutable directories to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
-
-	set -- su-exec elasticsearch "$@" ${es_opts}
-
+	set -- elasticsearch "$@"
 fi
 
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of user-mutable directories to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/logs
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/{data,logs}
 
-	set -- su-exec elasticsearch "$@" ${es_opts}
+	set -- su-exec elasticsearch "$@" "${es_opts[@]}"
 fi
 
 exec "$@"
